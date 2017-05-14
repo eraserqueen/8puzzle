6ade087c249fb5642d2c1e25016c3ee18ee2ece0
Dominique Rolin <drolin@thoughtworks.com>
2017-05-14 10:17:07 +1000

    compute next states from a given board

--- src/main/bfsSolver.py
+++ src/main/bfsSolver.py
@@ -1,11 +1,21 @@
+import copy
+
 from src.main.result import Result
+
 
 class BfsSolver:
     strategy = 'BFS'
 
-    def __init__(self, startingState):
-        print(f"running with {self.strategy}")
-        self.state = startingState
+    def __init__(self, starting_board):
+        self.state = starting_board
 
     def run(self):
         return Result()
+
+    def next_states(self, board):
+        states = []
+        for m in board.valid_moves:
+            next_state = copy.deepcopy(board)
+            getattr(next_state, m)()
+            states.append(next_state)
+        return states
--- src/test/testBfsSolver.py
+++ src/test/testBfsSolver.py
@@ -6,10 +6,24 @@ from src.main.board import Board
 
 class TestBfsSolver(unittest.TestCase):
     def test_init(self):
-        startingState = Board([0,1,2,3,4,5,6,7,8])
+        startingState = Board([0, 1, 2, 3, 4, 5, 6, 7, 8])
         solver = BfsSolver(startingState)
         self.assertEquals(startingState, solver.state)
 
+    def test_next_states(self):
+        board = Board([1, 2, 3, 4, 0, 5, 6, 7, 8])
+        solver = BfsSolver(board)
+        expected_states = [
+            Board([1, 0, 3, 4, 2, 5, 6, 7, 8]),
+            Board([1, 2, 3, 4, 7, 5, 6, 0, 8]),
+            Board([1, 2, 3, 0, 4, 5, 6, 7, 8]),
+            Board([1, 2, 3, 4, 5, 0, 6, 7, 8]),
+        ]
+        next_states = solver.next_states(board)
+        self.assertEquals(expected_states[0].tiles, next_states[0].tiles)
+        self.assertEquals(expected_states[1].tiles, next_states[1].tiles)
+        self.assertEquals(expected_states[2].tiles, next_states[2].tiles)
+        self.assertEquals(expected_states[3].tiles, next_states[3].tiles)
 
 if __name__ == '__main__':
     unittest.main()
